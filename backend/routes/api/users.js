59aef7be6f4efedf7251119be61cfcf14d0cd25a
Tristan C <47341361+PKTristan@users.noreply.github.com>
2023-06-29 16:01:20 -0700

    validate signup finished

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,19 +4,41 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 
 const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 const { User } = require('../../db/models');
 
-router.post('/', async (req, res, next) => {
-    const { firstName, lastName, email, username, password, avi } = req.body;
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    check('firstName')
+        .exists({ checkFalsy: true })
+        .isLength({min: 1, max: 20})
+        .withMessage('Please provide a first name between 1 and 20 characters.'),
+    check('lastName')
+        .exists({ checkFalsy: true})
+        .isLength({ min: 1, max: 20 })
+        .withMessage('Please provide a last name between 1 and 20 characters.'),
+    handleValidationErrors
+];
 
-    if (!password) {
-        const err = new Error('Password is required');
-        err.status = 400;
-        err.title = 'Password is required';
-        err.errors = { password: 'Password is required' };
-        return next(err);
-    }
+router.post('/', validateSignup, async (req, res, next) => {
+    const { firstName, lastName, email, username, password, avi } = req.body;
 
     const user = await User.create({
         firstName,
