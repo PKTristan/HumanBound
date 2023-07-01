2aad563c4548cb68d0098163931e3e365577d1f5
Tristan C <47341361+PKTristan@users.noreply.github.com>
2023-06-30 21:34:09 -0700

    finished create book now fixing bugs

--- backend/routes/api/books.js
+++ backend/routes/api/books.js
@@ -53,6 +53,10 @@ router.get('/', async (req, res, next) => {
         return next(err);
     }
 
+    books.forEach(book => {
+        book.authors = book.authors.split(',');
+    });
+
     return res.json({ books });
 });
 
@@ -80,6 +84,75 @@ router.get('/:id', async (req, res, next) => {
         err.errors = { book: 'No book found' };
         return next(err);
     }
+
+    return res.json({ book });
+});
+
+
+
+const validateBook = [
+    check('title')
+        .exists({ checkFalsy: true })
+        .isString()
+        .isLength({min: 1, max: 30})
+        .withMessage('Please provide a title.'),
+    check('authors')
+        .exists({ checkFalsy: true })
+        .isArray()
+        .notEmpty()
+        .withMessage('Please provide 1 or multiple authors in an array.'),
+    check('synopsis')
+        .exists({ checkFalsy: true })
+        .isString()
+        .isLength({ min: 20, max: Infinity })
+        .withMessage('Please provide a synopsis/summary of at least 20 words long.'),
+
+    // Optional fields
+    check('subtitle')
+        .optional()
+        .isString()
+        .isLength({ max: 50 })
+        .withMessage('Subtitle must be a string with a maximum length of 50 characters.'),
+    check('pdfLink')
+        .optional()
+        .isString()
+        .isURL()
+        .withMessage('Please provide a valid PDF link.'),
+    check('thumbnail')
+        .optional()
+        .isString()
+        .isURL()
+        .withMessage('Please provide a valid thumbnail URL.'),
+    check('pageCount')
+        .optional()
+        .isInt({ min: 1 })
+        .withMessage('Page count must be a positive integer.'),
+    check('publishYear')
+        .optional()
+        .isInt({ min: 1900, max: new Date().getFullYear() })
+        .withMessage('Publish year must be a valid year.'),
+
+
+    handleValidationErrors
+];
+
+
+//create a new book
+router.post('/', requireAuth, validateBook, async (req, res, next) => {
+    const { title, subtitle, authors, pdfLink, thumbnail, pageCount, publishYear, synopsis } = req.body;
+
+    const authStr = authors.join(',');
+
+    const book = await Book.create({
+        title,
+        subtitle,
+        authors: authStr,
+        pdfLink,
+        thumbnail,
+        pageCount,
+        publishYear,
+        synopsis
+    }).catch(err => next(err));
 
     return res.json({ book });
 });
