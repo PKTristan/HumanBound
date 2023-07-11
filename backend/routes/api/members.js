c4eb074b08af7d1331e9b8949041f351e7e8063b
Tristan C <47341361+PKTristan@users.noreply.github.com>
2023-07-10 21:45:41 -0700

    membership permissions

--- backend/routes/api/members.js
+++ backend/routes/api/members.js
@@ -7,12 +7,85 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { Op } = require('sequelize');
 
-const { Member, User } = require('../../db/models');
+const { Member, User, Circle } = require('../../db/models');
 
+
+const membershipPermission = async (req, res, next) => {
+    const { id } = req.params;
+    const { id: userId, admin } = req.user;
+
+    const membership = await Member.findByPk(id).catch(err => next(err));
+
+    if (!membership) {
+        const err = new Error('No membership found');
+        err.title = 'No membership found';
+        err.status = 404;
+        err.errors = { membership: 'No membership found' };
+        return next(err);
+    }
+
+    const currUserMembership = await Member.findAll({ where: { userId, circleId: membership.circleId } }).catch(err => next(err));
+
+    if (!admin && (!currUserMembership || !currUserMembership.length)) {
+        const err = new Error('Unauthorized');
+        err.title = 'Unauthorized';
+        err.status = 403;
+        err.errors = { membership: 'No membership found for current logged in user.' };
+        return next(err);
+    }
+
+    if (!admin && currUserMembership[0].status !== 'host') {
+        const err = new Error('Unauthorized');
+        err.title = 'Unauthorized';
+        err.status = 403;
+        err.errors = { membership: 'Unauthorized' };
+        return next(err);
+    }
+
+    const circle = await Circle.findByPk(membership.circleId).catch(err => next(err));
+    const user = await User.findByPk(membership.userId).catch(err => next(err));
+
+    if (!circle) {
+        const err = new Error('No circle found');
+        err.title = 'No circle found';
+        err.status = 404;
+        err.errors = { circle: 'No circle found' };
+        return next(err);
+    }
+
+    if (!user) {
+        const err = new Error('No user found');
+        err.title = 'No user found';
+        err.status = 404;
+        err.errors = { user: 'No user found' };
+        return next(err);
+    }
+
+    if (circle.creator === user.id || user.admin) {
+        const err = new Error('Unauthorized');
+        err.title = 'Unauthorized';
+        err.status = 403;
+        err.errors = { circle: 'user cannot be deleted as a member' };
+        return next(err);
+    }
+
+    return next();
+}
 
 // get all members of a circle
 router.get('/', restoreUser, requireAuth, async (req, res, next) => {
     const { id } = req.body;
+    const { id: userId, admin } = req.user;
+
+    const [member] = await findAll({ where: { circleId: id, userId } }).catch(err => next(err));
+
+    if (!admin && (!member || member.status === 'pending')) {
+        const err = new Error('No permission to view members of this group');
+        err.title = 'No permission to view members of this group';
+        err.status = 403;
+        err.errors = { member: 'No permission to view members of this group' };
+        return next(err);
+    }
 
     const members = await Member.findAll({
         where: {
@@ -84,7 +157,7 @@ const validateMember = [
 
 
 //edit a membership request
-router.put('/:id', restoreUser, requireAuth, validateMember, async (req, res, next) => {
+router.put('/:id', restoreUser, requireAuth, membershipPermission, validateMember, async (req, res, next) => {
     const { id } = req.params;
     const { status } = req.body;
 
@@ -105,7 +178,7 @@ router.put('/:id', restoreUser, requireAuth, validateMember, async (req, res, ne
 
 
 //delete a membership request
-router.delete('/:id', restoreUser, requireAuth, async(req, res, next) => {
+router.delete('/:id', restoreUser, requireAuth, membershipPermission, async(req, res, next) => {
     const { id } = req.params;
 
     const deleted = await Member.destroy({
